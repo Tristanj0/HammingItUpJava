9ceb2ac3d36fb02dd9cc94424ee9c5740a2779ae
tristan <jametri014@gmail.com>
2022-08-24 17:21:17 -0400

    This is a confusing one

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,20 @@
 package zipcode;
 
 public class Hamming {
+    String s1;
+    String s2;
     public Hamming(String s, String s1) {
+        this.s1 = s1;
+        this.s2 = s2;
+        if (s1.length() != s2.length())
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of legal length.");
     }
 
-    public int getHammingDistance() {
-        return -1;
+    public int getHammingDistance() throws IllegalArguementException {
+        int hammingDistance = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) hammingDistance++;
+        }
+        return hammingDistance;
     }
 }
